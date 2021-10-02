8d9f62072c1d6e1c39dfeba41b3b206d41b3288e
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2021-10-02 23:11:48 +0800

    新增uat環境配置，.gitignore加入dist

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ console.log(`環境：${process.env.NODE_ENV}`);
 console.log(`配置API位置：${process.env.VUE_APP_hostUrl}`);
 
 module.exports = {
-  productionSourceMap: process.env.NODE_ENV === 'production',
-  outputDir: process.env.outputDir, // 打包輸出目錄
+  productionSourceMap: process.env.NODE_ENV === 'sit',  // 只在sit產生source map
+  outputDir: process.env.outputDir,                     // 打包輸出目錄
   devServer: {
     proxy: {
       '/api': {
